27047216fcaa0fb97d10a967f1823d0689505c31
magnusa2007 <49444876+magnusa2007@users.noreply.github.com>
2024-02-26 21:12:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 import os,requests, sys
-pteroUrl = "url"
-api = 'ptlc_####'
-#From your account
+cookies={}
 
-XSRF = 'long string'
-#Get from header
+#Setup
+cookies['remember_web_#####'] = '####'
+#Get from DevTools ^^
+api = 'ptlc_#######'
+#From your account ^^^
+pteroUrl = "https://#######"
+#Setup end
 
-session = 'long string2'
-#Get from cookies
 
 headers = {
     "Authorization": api,
     "Accept": "application/json",
-    "x-xsrf-token": XSRF
+    'content-type': 'application/json'
 }
-cookies = {
-    'pterodactyl_session':session,
-    'XSRF-TOKEN':XSRF
-}
+
+r=requests.get(pteroUrl,headers=headers,cookies=cookies)
+responseCookies = r.cookies.get_dict()
+print(responseCookies['XSRF-TOKEN'])
+
+headers['x-xsrf-token'] = responseCookies['XSRF-TOKEN'][:-3]+'='
+cookies['pterodactyl_session']=responseCookies['pterodactyl_session']
+cookies['XSRF-TOKEN']=responseCookies['XSRF-TOKEN']
 
 r=requests.get(url=pteroUrl+'/api/client',headers=headers,cookies=cookies)
 data = r.json()
@@ -33,6 +38,7 @@ sys.stdout.write('> ')
 server=servers[int(input())]
 
 
+error = ['Run a command first']
 help = """help: This comand
 ls: Show everything in the folder.
 cd: Moves folder takes one argument(mods or ..).
@@ -41,7 +47,8 @@ rename: Takes two arguments(badmod.jar badmod.disabledjar).
 compress: Takes one argument(mods).
 decompress: Takes one argument(mods.tar.gz).
 exit: Closes the program.
-clear: Clears the console."""
+clear: Clears the console.
+error: Prints the context of the latest requests."""
 
 
 
@@ -51,6 +58,7 @@ def printfile(dir):
     for x in r.json()['data']:
         print(x['attributes']['name'])
     print(r)
+    error[0]=r.text
 
 def download(file,dir):
     url = pteroUrl+'/api/client/servers/'+server+'/files/download?file='+dir+'/'+file
@@ -61,41 +69,63 @@ def download(file,dir):
     with open(file, mode="wb") as fs:
         fs.write(r.content)
     print('Saved to '+file)
+    error[0]=r.text
 
 def delete(file,dir):
     url = pteroUrl+'/api/client/servers/'+server+'/files/delete'
     data = {"root":dir,"files":[file]}
     r = requests.post(url, headers=headers,json=data,cookies=cookies)
     print(r)
-
+    error[0]=r.text
+    
+def upload(file,dir):
+    fs = open(file,'rb').read()
+    files = {'file':fs}
+    url = pteroUrl+'/api/client/servers/'+server+'/files/upload'
+    r=requests.get(url,headers=headers,cookies=cookies)
+    print(r)
+    url = r.json()['attributes']['url']+dir
+    r = requests.post(url,headers=headers,cookies=cookies,files=files)
+    print(r)
+    error[0]=r.text
+    
 def compress(file,dir):
     url = pteroUrl+'/api/client/servers/'+server+'/files/compress'
     data = {"root":dir,"files":[file]}
     r = requests.post(url, headers=headers,json=data,cookies=cookies)
     print(r)
+    error[0] =r.text
 
 def decompress(file,dir):
     url = pteroUrl+'/api/client/servers/'+server+'/files/decompress'
     data = {"root":dir,"files":[file]}
     r = requests.post(url, headers=headers,json=data,cookies=cookies)
     print(r)
+    error[0] =r.text
 
 def rename(before,after,dir):
     url = pteroUrl+'/api/client/servers/'+server+'/files/rename'
     data= {'root': dir,'files':[{'from':before,'to':after}]}
     r = requests.put(url, json=data,headers = headers,cookies=cookies)
     print(r)
-    print(r.text)
+    error[0] =r.text
     
 def check(name,dir):
     url = pteroUrl+'/api/client/servers/'+server+'/files/list?directory='+dir
     r = requests.get(url,headers=headers,cookies=cookies)
+    error[0] =r.text
     for x in r.json()['data']:
         if name == x['attributes']['name']:
             return True
     return False
 
-directory=''  
+def areYouSure(text):
+    print(text)
+    print('yes/no')
+    sys.stdout.write('> ')
+    return input().lower() == 'yes'
+
+directory=''
 while True:
     sys.stdout.write('home'+directory+'> ')
     command = input()
@@ -126,7 +156,8 @@ while True:
     elif split[0] == 'rename':
         if check(split[1],directory):
             if len(split) == 3:
-                rename(split[1],split[2],directory)
+                if areYouSure('Are you sure that you want to rename '+split[1]):
+                    rename(split[1],split[2],directory)
             else:
                 print('Must be two arguments')
         else:
@@ -134,7 +165,8 @@ while True:
             
     elif split[0]=='delete':
         if check(split[1],directory):
-            delete(split[1],directory)
+            if areYouSure('Are you sure you want to delete '+split[1]+'\nTHIS CANNOT BE UNDONE!'):
+                delete(split[1],directory)
         else:
             print('Unkown file')
             
@@ -144,6 +176,10 @@ while True:
         else:
             print('Unkown file')
             
+    elif split[0]=='upload':
+        upload(split[1],directory)
+
+            
     elif split[0]=='decompress':
         if check(split[1],directory):
             decompress(split[1],directory)
@@ -152,5 +188,7 @@ while True:
             
     elif split[0] == 'help':
         print(help)
+    elif split[0] == 'error':
+        print(error[0])
     else:
         print('Unkown command do help for command list')
